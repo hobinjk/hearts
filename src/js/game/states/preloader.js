3867a9497220426206fb0707b3f09876e4be6342
James Hobin <hobinjk@mit.edu>
2015-02-15 01:49:14 -0500

    Remove unnecessary assets

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -2,12 +2,10 @@ function PreloaderState(game) {
   var preloader = {};
 
   preloader.preload = function() {
-    game.load.image('logo', 'images/phaser.png#grunt-cache-bust');
-
-    game.load.image('wall', 'images/wall.png');
-    game.load.image('block', 'images/block.png');
-    game.load.image('goal', 'images/goal.png');
-    game.load.image('empty', 'images/empty.png');
+    game.load.image('wall', 'images/wall.png#grunt-cache-bust');
+    game.load.image('block', 'images/block.png#grunt-cache-bust');
+    game.load.image('goal', 'images/goal.png#grunt-cache-bust');
+    game.load.image('empty', 'images/empty.png#grunt-cache-bust');
   };
 
   preloader.create = function() {
