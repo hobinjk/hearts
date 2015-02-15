6cce2e9adcead399b09c219e646eb41e66c20dde
James Hobin <hobinjk@mit.edu>
2015-02-15 01:51:11 -0500

    Further customize boilerplate

--- src/js/game/properties.js
+++ src/js/game/properties.js
@@ -1,15 +1,14 @@
 var npmProperties = require('../../../package.json');
 
 module.exports =
-  { title: 'Phaser JS Boilerplate'
+  { title: 'Hearts'
   , description: npmProperties.description
   , port: 3017
   , liveReloadPort: 3018
   , mute: false
-  , showStats: true
+  , showStats: false
   , size:
     { x: 800
     , y: 600
     }
-  , analyticsId: 'UA-50892214-2'
   };
